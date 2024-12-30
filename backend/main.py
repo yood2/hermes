1a22668a44f146f234d62f0a0b85a37c5403f7b3
yood2 <yood2@proton.me>
2024-12-29 18:26:42 -0800

    simple API route for adding a new user to persistence

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+USER_DATABASE = '../data/users.json'
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+def get_users():
+    try:
+        with open(USER_DATABASE, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_users(users):
+    with open(USER_DATABASE, 'w') as file:
+        json.dump(users, file)
+
+@app.post('/users')
+def create_user(user: User):
+    users = get_users()
+    users.append(user)
+    save_users(users)
+    return user
