4824ba1632a60d597df0f5700af90d78cb1f07f7
yood2 <yood2@proton.me>
2024-12-30 16:21:39 -0800

    basic cli

---
+++ cli/hermes_cli.py
@@ -0,0 +1,44 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:8000"
+
+def cli():
+    print('''
+
+         _       _    _            _          _   _         _           _        
+        / /\    / /\ /\ \         /\ \       /\_\/\_\ _    /\ \        / /\      
+       / / /   / / //  \ \       /  \ \     / / / / //\_\ /  \ \      / /  \     
+      / /_/   / / // /\ \ \     / /\ \ \   /\ \/ \ \/ / // /\ \ \    / / /\ \__  
+     / /\ \__/ / // / /\ \_\   / / /\ \_\ /  \____\__/ // / /\ \_\  / / /\ \___\ 
+    / /\ \___\/ // /_/_ \/_/  / / /_/ / // /\/________// /_/_ \/_/  \ \ \ \/___/ 
+   / / /\/___/ // /____/\    / / /__\/ // / /\/_// / // /____/\      \ \ \       
+  / / /   / / // /\____\/   / / /_____// / /    / / // /\____\/  _    \ \ \      
+ / / /   / / // / /______  / / /\ \ \ / / /    / / // / /______ /_/\__/ / /      
+/ / /   / / // / /_______\/ / /  \ \ \\/_/    / / // / /_______\\ \/___/ /       
+\/_/    \/_/ \/__________/\/_/    \_\/        \/_/ \/__________/ \_____\/        
+                                                                                 
+
+''')
+    input("Press Enter to continue...")
+    print('''
+    Welcome to Hermes CLI.
+
+    Select an option:
+    1. Create a new user
+    9. Exit
+''')
+    option = input("Enter an option: ")
+    if option == "1":
+        create_user()
+    elif option == "9":
+        exit()
+    else:
+        print("Invalid option")
+
+def create_user():
+    user_id = input("Enter a user ID: ")
+    response = requests.post(f"{BASE_URL}/users", json={"user_id": user_id})
+    print(response.json())
+
+if __name__ == "__main__":
+    cli()
